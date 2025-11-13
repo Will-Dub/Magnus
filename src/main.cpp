175d2d16f235660ac47ded62ffe78c7efc5bd81e
William Dubuc <william.dubuc01@gmail.com>
2025-11-13 01:05:43 -0500

    Base menu utilisateur

--- src/main.cpp
+++ src/main.cpp
@@ -4,17 +4,24 @@
 #include "Lcd.h"
 
 void getUserInput();
-void printNextMove();
+void waitForGameStart();
+void menuControlTick();
+void resetMenuIndex();
+void blinkWord(String text, unsigned int startWord1, unsigned int endWord1, unsigned int startWord2, unsigned int endWord2);
+void blinkChar(String text, int optionCount = 2);
 
 char selectedFromColChar = 'A';
 int selectedFromRow = 1;
 char selectedToColChar = 'A';
 int selectedToRow = 1;
-int currentIndex = 0;
+int currentHorizontalIndex = 0;
+int currentVerticalIndex = 0;
+bool isOkPressed = false;
 
 const int BLINK_TIME_MS = 500;
 unsigned long lastBlinkTime = 0;
 bool blinkState = true;
+bool isGameStarted = false;
 
 //-----------------------------
 // MAIN CODE.
@@ -27,82 +34,140 @@ void setup() {
 }
 
 void loop() {
+    if(!isGameStarted){
+        waitForGameStart();
+    }
+
     CHESS::printBoard();
     CHESS::printCurrentPlayer();
     getUserInput();
 }
 
-void printNextMove() {
+void waitForGameStart() {
+    resetMenuIndex();
+    LCD::print("Commencer");
+    LCD::print("      [OK]      ", 1);
+
+    while (!isOkPressed) {
+        menuControlTick();
+    }
+
+    resetMenuIndex();
+    LCD::print("Choix couleur");
+
+    while (!isOkPressed) {
+        blinkWord("[BLANC]   [NOIR]", 0, 6, 10, 15);
+        menuControlTick();
+    }
+
+    isGameStarted = true;
+}
+
+void blinkChar(String text, int optionCount) {
     unsigned long now = millis();
+    unsigned int index = currentHorizontalIndex % optionCount;
 
     if (now - lastBlinkTime > BLINK_TIME_MS) {
         blinkState = !blinkState;
         lastBlinkTime = now;
     }
 
-    String from = String(selectedFromColChar) + String(selectedFromRow);
-    String to = String(selectedToColChar) + String(selectedToRow);
-    String displayText = from + " a " + to;
+    if (!blinkState && index < text.length()) {
+        text.setCharAt(index, ' ');
+    }
+
+    LCD::print(text, 1);
+}
+
+void blinkWord(String text, unsigned int startWord1, unsigned int endWord1, unsigned int startWord2, unsigned int endWord2) {
+    unsigned long now = millis();
+    unsigned int index = currentHorizontalIndex % 2;
+
+    if (now - lastBlinkTime > BLINK_TIME_MS) {
+        blinkState = !blinkState;
+        lastBlinkTime = now;
+    }
 
     if (!blinkState) {
-        switch (currentIndex) {
-            case 0: displayText.setCharAt(0, ' '); break;
-            case 1: displayText.setCharAt(1, ' '); break;
-            case 2: displayText.setCharAt(5, ' '); break;
-            case 3: displayText.setCharAt(6, ' '); break;
+        unsigned int startWord = index == 0 ? startWord1 : startWord2;
+        unsigned int endWord = index == 0 ? endWord1 : endWord2;
+        
+        if (startWord >= 0 && endWord < text.length()) {
+            for (unsigned int i = startWord; i <= endWord; ++i) {
+                text[i] = ' ';
+            }
         }
     }
 
-    LCD::print(displayText, 1);
+    LCD::print(text, 1);
+}
+
+void resetMenuIndex(){
+    currentHorizontalIndex = 0;
+    currentVerticalIndex = 0;
+    isOkPressed = false;
+}
+
+void menuControlTick(){
+    /*if(isRightPressed){
+        currentHorizontalIndex++;
+        return;
+    }
+
+    if(isLeftPressed){
+        currentHorizontalIndex--;
+        return;
+    }
+
+    if(isUpPressed){
+        currentVerticalIndex++;
+    }
+
+    if(isDownPressed){
+        currentVerticalIndex--;
+    }
+        
+    if(isOkPressed){
+        isOkPressed = true
+    }*/
+
+    delay(100);
 }
 
 void getUserInput(){
-    LCD::print("Entrer votre mouvement: ");
+    // TODO: Ajout choix utilisateur
+    resetMenuIndex();
+    LCD::print("A votre tour");
 
-    while(currentIndex <= 3){
-        printNextMove();
-        /*if(rightPressed){
-            currentIndex++;
-            return;
-        }
+    while (!isOkPressed) {
+        blinkWord("[OK]  [Renoncer]", 0, 3, 6, 15);
+        menuControlTick();
+    }
 
-        if(leftPressed){
-            currentIndex--;
-            return;
-        }
+    resetMenuIndex();
+    LCD::print("De:");
 
-        if(upPressed){
-            switch(currentIndex){
-                case 0:
-                    break;
-                case 1:
-                    break;
-                case 2:
-                    break;
-                case 3:
-                    break;
-                default:
-                    break;
-            }
-        }
+    while(!isOkPressed){
+        //TODO: Change selected col + row
+        blinkChar(String(selectedFromColChar) + String(selectedFromRow));
+        menuControlTick();
+    }
 
-        if(downPressed){
-            switch(currentIndex){
-                case 0:
-                    break;
-                case 1:
-                    break;
-                case 2:
-                    break;
-                case 3:
-                    break;
-                default:
-                    break;
-            }
-        }
-        }*/
+    resetMenuIndex();
+    LCD::print("A:");
 
-        delay(100);
+    while(!isOkPressed){
+        //TODO: Change selected col + row
+        blinkChar(String(selectedToColChar) + String(selectedToRow));
+        menuControlTick();
+    }
+
+    resetMenuIndex();
+    LCD::print("Êtes-vous sûr");
+
+    while(!isOkPressed){
+        blinkWord("[OUI]      [NON]", 0, 4, 11, 15);
+        menuControlTick();
     }
 
     int selectedFromCol = CHESS::colCharToIndex(selectedFromColChar);
@@ -112,8 +177,16 @@ void getUserInput(){
 
     if(!result.isSuccess){
         String errorMessage = CHESS::getErrorMessage(result.erreur);
-        LCD::print(errorMessage, 1);
-        delay(2000);
+        Serial.println(errorMessage);
+
+        resetMenuIndex();
+        LCD::print("Coup invalide");
+        LCD::print("      [OK]      ", 1);
+
+        while(!isOkPressed){
+            menuControlTick();
+        }
+
         getUserInput();
         return;
     }
