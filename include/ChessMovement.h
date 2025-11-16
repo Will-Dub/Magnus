189c5ca28393f9e40c8ff5b7927b642fbf0aaecf
William Dubuc <william.dubuc01@gmail.com>
2025-11-16 02:37:15 -0500

    Chess movement moveFromOriginTo

--- include/ChessMovement.h
+++ include/ChessMovement.h
@@ -3,10 +3,10 @@
 #include <Arduino.h>
 #include "Movement.h"
 
-namespace CHESS_MOVEMENT{
-    int currentCol = 0;
-    int currentRow = 0;
+namespace CHESS_MOVEMENT
+{
     const float MAX_MOVE_DISTANCE = 99999;
+    const float ROBUS_RADIUS_CM = 15;
 
     // --------------------------------- 
     // 7   |   |   |   |   |   |   |   |
@@ -24,19 +24,27 @@ namespace CHESS_MOVEMENT{
     // 1   |   |   |   |   |   |   |   |
     // ---------------------------------
     // 0   |   |   |   |   |   |   |   |
-    // --0---1---2---3---4---5---6---7--
-    //              COL
+    // (O)0--1---2---3---4---5---6---7--
+    // (D)            COL
+    // (O)=Origin (D)=Dropoff
 
     /**
-     * @brief Ce dirige au milieu d'une case
+     * @brief Ce dirige au milieu d'une case(assume que le robot est à l'origine et regarde le nord)
      * 
-     * @param col entre 0-7
-     * @param row entre 0-7
+     * @param destCol entre 0-7
+     * @param destRow entre 0-7
      */
-    void moveTo(int col, int row);
-    void moveToDropOff();
-    void moveToWhite();
-    void moveToBlack();
+    void moveFromOriginTo(int destCol, int destRow);
+
+    /**
+     * @brief Bouge d'une case vers la zone de drop
+     * 
+     * @param currentCol entre 0-7
+     * @param currentRow entre 0-7
+     */
+    void moveFromSquareToDropOff(int currentCol, int currentRow);
+    //void moveToWhite();
+    //void moveToBlack();
 
     void waitEndMoveByLineNb(int nbLine);
 }
--- src/ChessMovement.cpp
+++ src/ChessMovement.cpp
@@ -1,17 +1,31 @@
 #include "ChessMovement.h"
 
 namespace CHESS_MOVEMENT{
-    void moveTo(int col, int row){
-        // Assume its facing north
-        MOVEMENT::moveForwardNonBlocking(MAX_MOVE_DISTANCE);
-        waitEndMoveByLineNb(row);
+    void moveFromOriginTo(int destCol, int destRow){
+        if(destCol < 0 || destRow < 0){
+            return;
+        }
 
-        MOVEMENT::turnRightNonBlocking(45);
+        // Ce rend SUR la ligne
+        if(destRow != 0){
+            MOVEMENT::moveForwardNonBlocking(MAX_MOVE_DISTANCE);
+            waitEndMoveByLineNb(destRow);
+            MOVEMENT::moveForward(ROBUS_RADIUS_CM);
+        }
+
+        // Tourne pour regarder l'EST
+        MOVEMENT::turnRight(30);
         MOVEMENT::turnRightUntilLine();
 
-        waitEndMoveByLineNb(col);
+        // Avance jusqu'au milieu de la case
+        MOVEMENT::moveForwardNonBlocking(MAX_MOVE_DISTANCE);
+        waitEndMoveByLineNb(destCol+1);
 
-        
+        // Tourne pour regarder le NORD
+        MOVEMENT::turnLeft(90);
+
+        // Avance jusqu'au milieu
+        MOVEMENT::moveUntilLine();
     }
 
     void waitEndMoveByLineNb(int nbLine){
@@ -26,6 +40,10 @@ namespace CHESS_MOVEMENT{
                 oldWasOnAllLine = true;
             }else if(LINE::ucReadLineSensors() != 0b111){
                 oldWasOnAllLine = false;
+            }
+
+            if(detectedLines >= nbLine){
+                MOVEMENT::stop();
             }
             
             delay(2);
